7fbec18d410600e09bb1efc1636da715d6122f4e
Arham Sheikh <162039821+arham771790@users.noreply.github.com>
2025-02-21 15:14:23 +0530

    Update axios.js

--- src/Lib/axios.js
+++ src/Lib/axios.js
@@ -1,10 +1,11 @@
-import axios from 'axios';
- const instance = axios.create({
-    baseURL: import.meta.env.MODE==='development'? 'http://localhost:5000/api':"/api",
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "https://chat-backend-h7p4.onrender.com/api",
     withCredentials: true,
     headers: {
-      'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
-  });
-  
-  export default instance;
+});
+
+export default instance;
